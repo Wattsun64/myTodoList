f57ef46b8013af18e6e57a486cc49bdff080018b
Wattsun64 <ecochra@gmail.com>
2017-04-12 11:40:23 -0400

    Changed functionality to remove element when clicked in main.js and minor adjustments to main.css

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,27 @@
 /**
  * Created by wattuns64 on 4/11/17.
  */
-(function() {
-    var textInput = document.getElementById('text-input'),  // Input where users will enter text
-        addBtn = document.getElementById('add'),            // Button when clicked will add text value
-        todo = document.getElementById('todo');             // The list container for all list items
 
-    todo.addEventListener('click', function(e) {    // Add event listener to parent div
-        e.target.classList.toggle('line-through');  // this will dish out click commands to all list-items
+(function() {
+    var txtInput = document.getElementById('text-input'),
+        btn = document.getElementById('add'),
+        todo = document.getElementById('todo');
+
+    btn.onclick = function () {
+        var txt = document.createTextNode(txtInput.value),
+            p = document.createElement('p');
+
+        p.appendChild(txt);
+        todo.appendChild(p);
+
+        txtInput.value = '';
+
+    }
+
+    todo.addEventListener('click', function (a) {
+        a.target.remove();
     })
 
-    addBtn.onclick = function() {                   // When user clicks button, it will add input text value
-        var listItem = document.createElement('p'),
-            task = document.createTextNode(textInput.value);
-
-        if ( textInput.value === '' ) {
-            alert('Please enter in a todo list item');  // If user does not add any text and clicks 'Add'
-        }                                               // They will be alerted that text is needed
-
-        listItem.appendChild(task);                     // Appending text value to new p element
-
-        todo.appendChild(listItem);                     // Appending p element to parent div
-
-        textInput.value = '';                           // Resetting text value to nothing
-    }
 
 
 }())
